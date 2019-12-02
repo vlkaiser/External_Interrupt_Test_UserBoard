3abb3a32a174cba5328382f9faf2995b7b1aae71
Vicki <vlkaiser9@gmail.com>
2019-12-02 17:15:05 -0500

    Updated: Replaced polling interrupt with callback.  Can be triggered at any point.

--- External_Interrupt_Test_MCU/src/main.c
+++ External_Interrupt_Test_MCU/src/main.c
@@ -5,11 +5,12 @@
 #include <asf.h>
 
 void configure_extint_channel(void);
+void configure_extint_callbacks(void);
+void extint_detection_callback(void);
 
 void configure_extint_channel(void)
 {
 	struct extint_chan_conf config_extint_chan;
-	
 	extint_chan_get_config_defaults(&config_extint_chan);
 	
 	config_extint_chan.gpio_pin = BUTTON_0_EIC_PIN;
@@ -19,14 +20,27 @@ void configure_extint_channel(void)
 	
 	extint_chan_set_config(BUTTON_0_EIC_LINE, &config_extint_chan);
 }
+void configure_extint_callbacks(void)
+{
+	extint_register_callback(extint_detection_callback, BUTTON_0_EIC_LINE, EXTINT_CALLBACK_TYPE_DETECT);
+	extint_chan_enable_callback(BUTTON_0_EIC_LINE, EXTINT_CALLBACK_TYPE_DETECT);
+}
+
+void extint_detection_callback(void)
+{
+	bool button_pin_state = port_pin_get_input_level(BUTTON_0_PIN);
+	port_pin_set_output_level(LED_0_PIN, button_pin_state);
+ }
 
 int main (void)
 {
 	system_init();
-	configure_extint_channel();	
+	configure_extint_channel();	configure_extint_callbacks();
+	system_interrupt_enable_global();
 	while (1)
 	{
-
+	// EXTINT will fire callback asynchronously.
+	/*
 		if (extint_chan_is_detected(BUTTON_0_EIC_LINE)) {
 			// Do something in response to EXTINT edge detection
 			
@@ -34,6 +48,6 @@ int main (void)
 			port_pin_set_output_level(LED_0_PIN, button_pin_state);
 			extint_chan_clear_detected(BUTTON_0_EIC_LINE);
 		}
-
+	*/
 	}
 }
